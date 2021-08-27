638fc47e3e74bd819cf33e694da5e7d139fb9375
s6252410014 <s6252410014@sau.ac.th>
2021-08-27 10:24:20 +0700

    Code V.1 : Do xxxx

---
+++ lib/views/first_ui.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class FirstUI extends StatelessWidget {
+  const FirstUI({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //  backgroundColor: Colors.amber,
+      appBar: AppBar(
+        title: Text(
+          'First UI ^_^',
+        ),
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Icon(
+          Icons.add,
+          color: Colors.amber[800],
+        ),
+      ),
+      // floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Supapol Homjan',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.green[800]
+              ),
+            ),
+            Text(
+              ' 6252410014 ',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.green[900]
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
